261bb1af7f8d91bf9ea90963b1e17bf234e54326
Kunal Singh <46413519+ks1912@users.noreply.github.com>
2020-03-12 02:39:35 +0530

    Update hc_insurance.py

--- hc_insurance.py
+++ hc_insurance.py
@@ -25,8 +25,8 @@ y_hc=hc.fit_predict(X)
 
 #Visualising the clusters
 # If u are using more then 2 dimension don't execute this portion of code as it is valid in 2d only
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Steady')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Intellegent')
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Age')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Price')
 plt.title('Clusters of customers')
 plt.xlabel('Age')
 plt.ylabel('Charges')
